35dbe407078c9c5355ce98355552eccf2bd07a28
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-21 01:30:16 +0900

    CRUD 요청 템플릿 수정 / 2024-05-21

--- src/main/java/com/example/demo/RestAPI.java
+++ src/main/java/com/example/demo/RestAPI.java
@@ -14,17 +14,17 @@ public class RestAPI {
 		return "getall";
 	}
 	// json을 받기 때문에 @RequestBody Book book을 적어줌
-	@RequestMapping(method = RequestMethod.POST, path = "/add")
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.GET}, path = "/add")
 	public String Add(@RequestBody Book book) {
 		return "Add";
 	}
 	// @PathVariable를 사용하면 동적 path를 자동으로 파싱한다.
-	@RequestMapping(method = RequestMethod.PUT, path = "/update/{id}")
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.GET}, path = "/update/{id}")
 	public String Update(@RequestBody Book book, @PathVariable int id) {
 		return "Update";
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{id}")
+	@RequestMapping(method = {RequestMethod.DELETE, RequestMethod.GET}, path = "/delete/{id}")
 	public String Delete(@PathVariable int id) {
 		return "Delete";
 	}
