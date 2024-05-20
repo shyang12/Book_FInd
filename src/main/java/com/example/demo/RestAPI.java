ee150567bc5c692fe5d3da650d6e43971d44931e
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-20 22:54:44 +0900

    CRUD 요청 템플릿 작성 / 2024-05-20

---
+++ src/main/java/com/example/demo/RestAPI.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestAPI {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/books")
+	public String GetAll() {
+		return "getall";
+	}
+	// json을 받기 때문에 @RequestBody Book book을 적어줌
+	@RequestMapping(method = RequestMethod.POST, path = "/add")
+	public String Add(@RequestBody Book book) {
+		return "Add";
+	}
+	// @PathVariable를 사용하면 동적 path를 자동으로 파싱한다.
+	@RequestMapping(method = RequestMethod.PUT, path = "/update/{id}")
+	public String Update(@RequestBody Book book, @PathVariable int id) {
+		return "Update";
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{id}")
+	public String Delete(@PathVariable int id) {
+		return "Delete";
+	}
+}
