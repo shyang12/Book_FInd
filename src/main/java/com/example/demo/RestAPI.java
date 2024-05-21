bde0de59c3fe4e14c5d5c7710cdc3eb71f6b5250
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-21 23:33:09 +0900

    CRUD 로직 구현 / 2024-05-21

--- src/main/java/com/example/demo/RestAPI.java
+++ src/main/java/com/example/demo/RestAPI.java
@@ -6,26 +6,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 public class RestAPI {
 
+	private List<Book> bookList = new ArrayList<>();
+	
 	@RequestMapping(method = RequestMethod.GET, path = "/books")
-	public String GetAll() {
-		return "getall";
+	public List<Book> GetAll() {
+		return bookList;
 	}
 	// json을 받기 때문에 @RequestBody Book book을 적어줌
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.GET}, path = "/add")
 	public String Add(@RequestBody Book book) {
+		bookList.add(book);
 		return "Add";
 	}
 	// @PathVariable를 사용하면 동적 path를 자동으로 파싱한다.
 	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.GET}, path = "/update/{id}")
-	public String Update(@RequestBody Book book, @PathVariable int id) {
-		return "Update";
+	public String Update(@RequestBody Book tobook, @PathVariable int id) {
+		
+		Book find_book = bookList.stream()
+				.filter(book -> book.getId() == id)
+				.findAny()
+				.orElse(null);
+		
+		if (find_book != null) {
+			find_book.setAuthor(tobook.getAuthor());
+			find_book.setName(tobook.getName());
+			find_book.setPrice(tobook.getPrice());
+			
+			return "success";
+		}
+		return "not valid";
 	}
 	
 	@RequestMapping(method = {RequestMethod.DELETE, RequestMethod.GET}, path = "/delete/{id}")
 	public String Delete(@PathVariable int id) {
-		return "Delete";
+		
+		Book find_book = bookList.stream()
+				.filter(book -> book.getId() == id)
+				.findAny()
+				.orElse(null);
+		
+		if (find_book != null) {
+			bookList.remove(find_book);
+			return "success";
+		}
+		
+		return "not valid";
 	}
 }
